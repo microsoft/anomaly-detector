ffe4a02c04954e0dff47e72859c9515a8e975223
Duan Juanyong <juanyong.duan@microsoft.com>
2024-03-29 14:24:14 +0800

    fix setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,8 @@
 
 from setuptools import setup, Extension
 
-class get_numpy_include(object):
+
+class GetNumpyInclude(object):
     """A lazy include path for numpy.
     This way numpy isn't imported until it's actually installed,
     so the `install_requires` argument can handle it properly.
@@ -16,11 +17,11 @@ class get_numpy_include(object):
 
 if __name__ == "__main__":
     setup(
-        include_dirs=[get_numpy_include()],
         ext_modules=[
             Extension(
                 "anomaly_detector.univariate._anomaly_kernel_cython",
                 ["src/anomaly_detector/univariate/_anomaly_kernel_cython.c"],
+                include_dirs=[GetNumpyInclude()]
             )
         ]
     )
