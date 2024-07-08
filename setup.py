201d161c0e0a56786e3a7deeaf4bcfe7d0ef73c4
Duan Juanyong <juanyong.duan@microsoft.com>
2024-07-08 14:29:11 +0800

    change job name
    change job name
    
    remove cython c source. build from cython code
    
    change setup.py
    
    add dep in ci/cd
    
    install dep
    
    add dep in toml
    
    add versions of dep
    
    refine setup.py
    
    change cython version to 3.0.10
    
    remove python matrix in cd. no need to use multiple versions of python
    
    disable building PyPy wheels
    
    test
    
    fix bug
    
    test publish pipeline
    
    udpate workflow
    
    fix bug
    
    update version
    
    update version

--- setup.py
+++ setup.py
@@ -3,26 +3,20 @@
 # ---------------------------------------------------------
 
 from setuptools import setup, Extension
-
-
-class GetNumpyInclude(object):
-    """A lazy include path for numpy.
-    This way numpy isn't imported until it's actually installed,
-    so the `install_requires` argument can handle it properly.
-    """
-    def __str__(self):
-        import numpy
-        return numpy.get_include()
+from Cython.Build import cythonize
+import numpy as np
 
 
 if __name__ == "__main__":
+    extensions = [
+        Extension(
+            "anomaly_detector.univariate._anomaly_kernel_cython",
+            ["src/anomaly_detector/univariate/_anomaly_kernel_cython.pyx"],
+            include_dirs=[np.get_include()]
+        )
+    ]
+
     setup(
         setup_requires=["numpy"],
-        ext_modules=[
-            Extension(
-                "anomaly_detector.univariate._anomaly_kernel_cython",
-                ["src/anomaly_detector/univariate/_anomaly_kernel_cython.c"],
-                include_dirs=[GetNumpyInclude()]
-            )
-        ]
+        ext_modules=cythonize(extensions)
     )
